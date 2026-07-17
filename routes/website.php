bffe62dd2758578b33a441d92f849c49184bedad
durairaj86 <vcdurai@gmail.com>
2026-07-17 09:24:31 +0530

    first commit

---
+++ routes/website.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Website\ContactController;
+
+Route::name('website.')->group(function () {
+    Route::view('/', 'website.home')->name('home');
+    Route::view('/features', 'website.features')->name('features');
+    Route::view('/services', 'website.services')->name('services');
+    Route::view('/products', 'website.products')->name('products');
+    Route::view('/pricing', 'website.pricing')->name('pricing');
+    Route::get('/contact', [ContactController::class, 'show'])->name('contact');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.submit');
+});
