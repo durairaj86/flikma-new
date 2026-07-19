60a962a6f02b8d0a27a885fc0cdbc0a734fd1290
durairaj86 <vcdurai@gmail.com>
2026-07-19 12:19:07 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
+    dd(request()->getHost());
     if (request()->getHost() === 'app.flikma.com') {
         return redirect('/login');
     }
